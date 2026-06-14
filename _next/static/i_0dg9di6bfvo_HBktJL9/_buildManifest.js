9d919d64f1c6413ca8458c2fd19a404c9765ed37
Aiden DeForte <aidendeforte@gmail.com>
2026-06-13 20:14:18 -0400

    SEO: add title/description/canonical for vs-prism via segment layout

---
+++ _next/static/i_0dg9di6bfvo_HBktJL9/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/logicinsight-next//_next/:path+",
+        "destination": "/logicinsight-next/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
